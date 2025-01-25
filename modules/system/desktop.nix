793bf4f30ab2d1899ebaf2f6d3d85433009ce8d4
gaoutaman <g.man.9931@gmail.com>
2025-01-25 13:58:27 +0000

    format

--- modules/system/desktop.nix
+++ modules/system/desktop.nix
@@ -13,7 +13,7 @@
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   services.displayManager.sddm = {
-  	enable = true;
-	wayland.enable = true;
+    enable = true;
+    wayland.enable = true;
   };
 }
